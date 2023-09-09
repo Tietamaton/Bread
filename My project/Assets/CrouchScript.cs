4877d061152f4fa7528579e018fad557ce27d9ff
Tietamaton <144351216+Tietamaton@users.noreply.github.com>
2023-09-09 20:22:01 +0300

    Muutoksia muutoksia
    Co-Authored-By: Makkeeee <144351521+Makkeeee@users.noreply.github.com>
    Co-Authored-By: kuutti77 <144452621+kuutti77@users.noreply.github.com>

---
+++ My project/Assets/CrouchScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrouchScript : MonoBehaviour
+{
+	public CharacterController PlayerHeight;
+	public CapsuleCollider playerCol;
+	public float normalHeight, crouchHeight;
+	public Transform player;
+	public Vector3 offset;
+	
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.C)){
+			PlayerHeight.height = crouchHeight;
+			playerCol.height = crouchHeight;
+		}
+		if(Input.GetKeyUp(KeyCode.C)){
+			PlayerHeight.height = normalHeight;
+			playerCol.height = normalHeight;
+            player.position = player.position + offset;			
+		}
+	}
+}
